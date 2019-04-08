a1ef175249f989245d42336d6b1a22b2c171f5f7
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-04-08 18:00:20 +0200

    Add if's to the main.js

--- main.js
+++ main.js
@@ -3,22 +3,37 @@
 //Recoger la info del input
 const inputEl = document.querySelector('.number__input');
 //Recoger la info del botón
-const ButtonEl = document.querySelector('.button')
+const buttonEl = document.querySelector('.button');
+//Recoger la caja de pistas
+const hintEl = document.querySelector('.hint__text');
+//Declarar número aleatorio
+let randomNumber = 0;
+
 //Función generadora de número aleatorio
 function getRandomNumber() {
-    return Math.ceil(Math.random() * 100);
+     randomNumber = Math.ceil(Math.random() * 100);
+     return 'El número generado es ' + randomNumber;
   }
+
+console.log(getRandomNumber());
 
 //Función de mostrar contenido del input
 function handleButtonClick(event){
     event.preventDefault();
-    const writtenNumber = inputEl.value
-    console.log ('El número escrito es ' + writtenNumber);
-}
+    const writtenNumber = inputEl.value;
+    console.log('El número escrito es ' + writtenNumber);
+    if (randomNumber > writtenNumber){
+        hintEl.innerHTML = 'Tu número es menor';
+    }
+    else if (randomNumber < writtenNumber){
+        hintEl.innerHTML = 'Tu número es mayor';
+    }
+    else if (randomNumber = writtenNumber){
+        hintEl.innerHTML = 'ACERTASTE';
+    }
+};
 
 //Añadir listener al botón
-ButtonEl.addEventListener('click', handleButtonClick);
+buttonEl.addEventListener('click', handleButtonClick);
 
 
-
-console.log(getRandomNumber());
