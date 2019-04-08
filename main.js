953091cee3c2b99e0f778f5f412c3adb5c1c3e21
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-04-08 17:32:41 +0200

    Add random number generator, variables and eventprevent in handler

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('Javascript link');
+//Recoger la info del input
+const inputEl = document.querySelector('.number__input');
+//Recoger la info del botón
+const ButtonEl = document.querySelector('.button')
+//Función generadora de número aleatorio
+function getRandomNumber() {
+    return Math.ceil(Math.random() * 100);
+  }
+
+//Función de mostrar contenido del input
+function handleButtonClick(event){
+    event.preventDefault();
+    const writtenNumber = inputEl.value
+    console.log ('El número escrito es ' + writtenNumber);
+}
+
+//Añadir listener al botón
+ButtonEl.addEventListener('click', handleButtonClick);
+
+
+
+console.log(getRandomNumber());
